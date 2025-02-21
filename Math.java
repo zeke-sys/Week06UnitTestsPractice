e224ef12b14a4ea785373fe8898adb7be141f9c7
zeke-sys <zek3.isaac@gmail.com>
2025-02-20 22:22:09 -0700

    first commit

---
+++ Math.java
@@ -0,0 +1,18 @@
+package week06UnitTestsPractice;
+
+public class Math {
+	
+	public int multiply(int a, int b) { //method that multiplies two ints and returns an int
+		return a * b;
+	}
+	
+	public double divide(int a, int b) { //method that divides two doubles and returns a double
+		return a / b;
+	}
+	
+	public boolean isPositiveNumber(int a) { //method that returns true if int is positive
+		return a > 0;
+	}
+	
+	
+} //end of class
---
+++ MathTest.java
@@ -0,0 +1,40 @@
+package week06UnitTestsPractice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+//import week06UnitTestsPractice.Math; //Can't import because MathTest is in the same package of the Math class.
+
+class MathTest {
+	
+	Math math = new Math();
+
+	@Test
+	void testMultiplyReturnsCorrectValues() {
+		int product = math.multiply(2, 5);
+		assertEquals(10, product);
+	}
+	
+	@Test
+	void testDivideReturnsCorrectValues() {
+		double quotient = math.divide(5, 2);
+		assertEquals(2, quotient);
+	}
+	
+	@Test
+	void testIsPositiveReturnsTrueIfArgIsPositive() {
+		int a = 5;
+		assertTrue(math.isPositiveNumber(a));
+	}
+	
+	@Test
+	 void testDivideThrowsExceptionWhenDividingbyZero() {
+		assertThrows(ArithmeticException.class, () -> math.divide(5, 0)); //using lambda () ->
+	}
+	
+	
+	
+
+
+}
